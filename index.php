7e36ab07943c245f17ba8bcb0812b72b7b20740c
alha13 <alhassaneo@gmail.com>
2019-02-15 22:10:35 +0000

    Update index.php

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ if($method == 'POST'){
 	switch ($text) {
 		case '11111111':
 			// $speech = "Hi, Nice to meet you";
-			 $speech = "Liste des *pharmacies de garde à Bouaflé*.\n_____________________________________ https://www.numelion.com/wp-content/uploads/2013/09/comment-utiliser-json-dans-php.jpg \n Ouattara";
+			 $speech = "Liste des *pharmacies de garde à Bouaflé*.\n_____________________________________ \n https://www.numelion.com/wp-content/uploads/2013/09/comment-utiliser-json-dans-php.jpg \n Ouattara";
 
 			break;
 
