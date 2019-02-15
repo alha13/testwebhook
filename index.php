0705e5d52474fadfdc467f72284d6c15bc3022d0
Ouattara Zie Alhassane <35994379+alha13@users.noreply.github.com>
2019-02-15 20:39:01 +0000

    Update index.php

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ if($method == 'POST'){
 	switch ($text) {
 		case '11111111':
 			$speech = "Hi, Nice to meet you";
-			 $speech = "https://www.youtube.com/watch?v=RjHQOzC9eyU";
+			 $speech = "https://www.numelion.com/wp-content/uploads/2013/09/comment-utiliser-json-dans-php.jpg";
 
 			break;
 
