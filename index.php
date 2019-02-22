021f784cce3a327ea2be6d227e41ec0e379f10c9
alha13 <alhassaneo@gmail.com>
2019-02-22 18:04:18 +0000

    Update index.php

--- index.php
+++ index.php
@@ -55,18 +55,15 @@ if($method == 'POST'){
 	// "speech": "this text is spoken out loud if the platform supports voice interactions",
 	// "displayText": "this text is displayed visually",
 
-	$body = '{
-	  "type": 1,
-	  "title": "card title",
-	  "subtitle": "card text",
-	  "imageUrl": "https://assistant.google.com/static/images/molecule/Molecule-Formation-stop.png"
-	}';
 	//$response->source = $source;
 	//echo $body;
 	$response->speech = $speech;
 	$response->displayText = $speech;
 
 	//$response->messages = $body;
+	$response->messages->type = "1";
+	$response->messages->title = "TITRE";
+	$response->messages->subtitle = "sous titre";
 	$response->messages->imageUrl = "https://assistant.google.com/static/images/molecule/Molecule-Formation-stop.png";
 
 	$response->source = "webhook";
