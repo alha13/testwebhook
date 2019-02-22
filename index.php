e83edfc1c0d71bfef13e7adc6501360a47b3bf0e
alha13 <alhassaneo@gmail.com>
2019-02-22 17:33:43 +0000

    Update index.php

--- index.php
+++ index.php
@@ -56,20 +56,19 @@ if($method == 'POST'){
 	// {
 	// "speech": "this text is spoken out loud if the platform supports voice interactions",
 	// "displayText": "this text is displayed visually",
-	// "messages": {
-	//   "type": 1,
-	//   "title": "card title",
-	//   "subtitle": "card text",
-	//   "imageUrl": "https://assistant.google.com/static/images/molecule/Molecule-Formation-stop.png"
-	// }
 
+	$body = '"messages": {
+	  "type": 1,
+	  "title": "card title",
+	  "subtitle": "card text",
+	  "imageUrl": "https://assistant.google.com/static/images/molecule/Molecule-Formation-stop.png"
+	}'
 	//$response->source = $source;
 
 	$response->speech = $speech;
 	$response->displayText = $speech;
 
-	$response->imageUrl = $data;
-	$response->imageUrl = $imageUrl;
+	$response->imageUrl = $body;
 
 	$response->source = "webhook";
 	echo json_encode($response);
