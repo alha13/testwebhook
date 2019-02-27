a9edc04ca3628ebe77584649a63b39b99c6ff8bc
alha13 <alhassaneo@gmail.com>
2019-02-27 22:32:52 +0000

    Update index.php

--- index.php
+++ index.php
@@ -16,17 +16,18 @@ if($method == 'POST'){
 
 	//echo $text;
 	//$GET['enter'] = 'AZO';
-	// $sql1 = "SELECT * FROM `azo` WHERE `nom` LIKE :nom LIMIT 50";
-	//
-	// 				$query1 = $bd->prepare($sql1);
-	// 				$query1->execute(array(
-	// 					'nom' => $text,
-	// 				));
 
-					$sql1 = "SELECT * FROM `azo` WHERE `nom` LIKE ?% LIMIT 50";
+	$sql1 = "SELECT * FROM `azo` WHERE `nom` LIKE :nom LIMIT 50";
 
-									$query1 = $bd->prepare($sql1);
-									$query1->execute(array( $text	));
+					$query1 = $bd->prepare($sql1);
+					$query1->execute(array(
+						'nom' => $text."%",
+					));
+					//
+					// $sql1 = "SELECT * FROM `azo` WHERE `nom` LIKE ? LIMIT 50";
+					//
+					// 				$query1 = $bd->prepare($sql1);
+					// 				$query1->execute(array( $text	));
 
 // echo $aut_resultat['prenom'];
 
