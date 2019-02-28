77cd4ce568e51a9090906354860b4d10a5fa367f
alha13 <alhassaneo@gmail.com>
2019-02-28 00:05:37 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 include('database.php');
 
-		//echo $aut_resultat['id'];
+
 	?>
 
 <?php
@@ -12,24 +12,20 @@ if($method == 'POST'){
 	$requestBody = file_get_contents('php://input');
 	$json = json_decode($requestBody);
 
+//recuperation des variable recu
 	$text = $json->result->parameters->text;
+	$cle = $json->result->parameters->cle;
 
-	//echo $text;
-	//$GET['enter'] = 'AZO';
 
-	$sql1 = "SELECT * FROM `Prix_medicamens` WHERE `denomination` LIKE :nom LIMIT 50";
+//mise en execution des requettes pour la recuperation de donnees BD
 
-					$query1 = $bd->prepare($sql1);
-					$query1->execute(array(
-						'nom' => $text."%",
-					));
-					//
-					// $sql1 = "SELECT * FROM `azo` WHERE `nom` LIKE ? LIMIT 50";
-					//
-					// 				$query1 = $bd->prepare($sql1);
-					// 				$query1->execute(array( $text	));
+//debut recupration prix de medicamants
+$sql1 = "SELECT * FROM `Prix_medicamens` WHERE `denomination` LIKE :nom LIMIT 50";
 
-// echo $aut_resultat['prenom'];
+				$query1 = $bd->prepare($sql1);
+				$query1->execute(array(
+					'nom' => $text."%",
+				));
 
 	switch ($text) {
 
@@ -51,31 +47,41 @@ if($method == 'POST'){
 		default:
 			$speech = "Désolé, je n'ai pas compris ça. S'il vous plaît demandez-moi quelque chose d'autre.";
 			break;
-
-
 	}
+	//fin recupration prix de medicamants
+
+
+
+	//debut recupration meteo
+	switch ($cle) {
+
+			case 'azo':
+			$speech = "Yes, you can type anything here.";
+			break;
+
+
+		default:
+			$speech = "Désolé, je n'ai pas compris ça. S'il vous plaît demandez-moi quelque chose d'autre.";
+			break;
+	}
+	//fin recupration meteo
+
 
 	$response = new \stdClass();
 
-	// {
-	// "speech": "this text is spoken out loud if the platform supports voice interactions",
-	// "displayText": "this text is displayed visually",
-
-	//$response->source = $source;
-	//echo $body;
 	$response->speech = $speech;
 	$response->displayText = $speech;
-	// $response->messages->type = "1";
-	// $response->messages->title = "TITRE";
-	// $response->messages->subtitle = "sous titre";
-	// $response->messages->imageUrl = "https://assistant.google.com/static/images/molecule/Molecule-Formation-stop.png";
 
 	$response->source = "Mon_Môgô";
+
+
+
 	echo json_encode($response);
 }
 else
 {
 	echo " <br> Acees a ghit mais pas au case";
 }
+
 
 ?>
